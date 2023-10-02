0280fcf652ab3541c0d51d313ab94636fd81c7b7
Lollija <lollija.lukasova@gmail.com>
2023-10-02 12:43:26 +0000

    small changes to wording

--- main.py
+++ main.py
@@ -111,11 +111,11 @@ while space_exist():
         continue
     #
     if won:
-        # TODO
+        result = 'Congrats! You won'
 
         break
     elif computer_move()[1]:
-        result = '=== You lose ! =='
+        result = 'You lose !'
         break
 print_board()
 print(result)
